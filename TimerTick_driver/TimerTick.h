9219274a0962c17d2adb260755093fabb3ecbb46
Mourad Rashwan <32272179+MouradRashwan@users.noreply.github.com>
2019-06-12 12:22:13 +0200

    Add files via upload

--- TimerTick_driver/TimerTick.h
+++ TimerTick_driver/TimerTick.h
@@ -8,7 +8,7 @@
 #ifndef TIMERTICK_H_
 #define TIMERTICK_H_
 
-void TimerTick_init(uint32_t ui32TickMS, void (*pfnTickISR)(void));
+void TimerTick_init(uint32_t ui32TickPeriodInMillis, void (*pfnCallBack)(void));
 
 void TimerTick_start(void);
 
